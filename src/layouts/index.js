551525146fa18aef91e5a7e43227892d9a4f32fc
Cliff Pyles <cliff@cliffpyles.com>
2018-01-26 08:36:08 -0500

    Clear content

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
-import Navbar from '../components/Navbar';
 import './all.sass';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
-    <Navbar />
+    <Helmet title="CSS Layouts" />
     <div>{children()}</div>
   </div>
 );
