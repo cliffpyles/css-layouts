55e0d0d74a935d754b5af7e5a972d1299cacfb6f
Cliff Pyles <cliff@cliffpyles.com>
2018-01-31 07:47:17 -0500

    Move workspace data to seperate file

---
+++ src/data.js
@@ -0,0 +1,69 @@
+export default {
+  someTrueValue: true,
+  someFalseValue: false,
+  breadcrumbItems: [
+    { href: '/', text: 'Design System' },
+    { href: '/components', text: 'components' },
+    { href: '/examples', text: 'examples' },
+    { href: '/layouts', text: 'layouts' }
+  ],
+  menuItems: [
+    { href: '/', text: 'Design System' },
+    {
+      href: '/general',
+      text: 'general',
+      children: [
+        {
+          href: '/general/dashboard',
+          text: 'dashboard'
+        },
+        {
+          href: '/general/settings',
+          text: 'settings'
+        }
+      ]
+    },
+    {
+      href: '/administration',
+      text: 'administration',
+      children: [
+        {
+          href: '/administration/teams',
+          text: 'teams'
+        },
+        {
+          href: '/administration/customers',
+          text: 'customers'
+        }
+      ]
+    }
+  ],
+  navItems: [
+    { href: '/', text: 'Design System' },
+    { href: '/components', text: 'components' },
+    { href: '/examples', text: 'examples' },
+    { href: '/layouts', text: 'layouts' }
+  ],
+  paginationItems: [
+    { href: '/home1', text: 'Design System' },
+    { href: '/components1', text: 'components' },
+    { href: '/examples1', text: 'examples' },
+    { href: '/layouts1', text: 'layouts' },
+    { href: '/home2', text: 'Design System' },
+    { href: '/components2', text: 'components' },
+    { href: '/examples2', text: 'examples' },
+    { href: '/layouts2', text: 'layouts' },
+    { href: '/home3', text: 'Design System' },
+    { href: '/components3', text: 'components' },
+    { href: '/examples3', text: 'examples' },
+    { href: '/layouts3', text: 'layouts' },
+    { href: '/home4', text: 'Design System' },
+    { href: '/components4', text: 'components' },
+    { href: '/examples4', text: 'examples' },
+    { href: '/layouts4', text: 'layouts' },
+    { href: '/home5', text: 'Design System' },
+    { href: '/components5', text: 'components' },
+    { href: '/examples5', text: 'examples' },
+    { href: '/layouts5', text: 'layouts' },
+  ],
+}
--- src/pages/workspace.js
+++ src/pages/workspace.js
@@ -13,78 +13,12 @@ import DsMenu from '../components/DsMenu';
 import DsNav from '../components/DsNav';
 import DsPagination from '../components/DsPagination';
 
+import data from '../data';
+
 export default class IndexPage extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {
-      someTrueValue: true,
-      someFalseValue: false,
-      breadcrumbItems: [
-        { href: '/', text: 'Design System' },
-        { href: '/components', text: 'components' },
-        { href: '/examples', text: 'examples' },
-        { href: '/layouts', text: 'layouts' }
-      ],
-      menuItems: [
-        { href: '/', text: 'Design System' },
-        {
-          href: '/general',
-          text: 'general',
-          children: [
-            {
-              href: '/general/dashboard',
-              text: 'dashboard'
-            },
-            {
-              href: '/general/settings',
-              text: 'settings'
-            }
-          ]
-        },
-        {
-          href: '/administration',
-          text: 'administration',
-          children: [
-            {
-              href: '/administration/teams',
-              text: 'teams'
-            },
-            {
-              href: '/administration/customers',
-              text: 'customers'
-            }
-          ]
-        }
-      ],
-      navItems: [
-        { href: '/', text: 'Design System' },
-        { href: '/components', text: 'components' },
-        { href: '/examples', text: 'examples' },
-        { href: '/layouts', text: 'layouts' }
-      ],
-      paginationItems: [
-        { href: '/home1', text: 'Design System' },
-        { href: '/components1', text: 'components' },
-        { href: '/examples1', text: 'examples' },
-        { href: '/layouts1', text: 'layouts' },
-        { href: '/home2', text: 'Design System' },
-        { href: '/components2', text: 'components' },
-        { href: '/examples2', text: 'examples' },
-        { href: '/layouts2', text: 'layouts' },
-        { href: '/home3', text: 'Design System' },
-        { href: '/components3', text: 'components' },
-        { href: '/examples3', text: 'examples' },
-        { href: '/layouts3', text: 'layouts' },
-        { href: '/home4', text: 'Design System' },
-        { href: '/components4', text: 'components' },
-        { href: '/examples4', text: 'examples' },
-        { href: '/layouts4', text: 'layouts' },
-        { href: '/home5', text: 'Design System' },
-        { href: '/components5', text: 'components' },
-        { href: '/examples5', text: 'examples' },
-        { href: '/layouts5', text: 'layouts' },
-      ],
-    };
+    this.state = data;
   }
 
   handleScriptLoad() {
